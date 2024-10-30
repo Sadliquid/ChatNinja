dfaa4003669b436a5e6d329c06461754abc6eae7
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-10-30 21:35:09 +0800

    Added access control

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ bot.once('ready', async () => {
                 name: 'end',
                 description: 'End chat session',
             },
+            {
+                name: 'config',
+                description: 'Setup your ChatNinja experience',
+            }
         ]);
     });
 
@@ -54,12 +58,6 @@ bot.once('ready', async () => {
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_SECRET_KEY });
 
 bot.on('interactionCreate', async (interaction) => {
-    if (process.env.DISABLE_API_KEY === "true") {
-        if (interaction.user.id !== bot.user.id) {
-            await interaction.reply({ content: 'API Key has been disabled. Please try again later.', ephemeral: true });
-        }
-        return;
-    }
     if (!interaction.isCommand()) return;
 
     if (interaction.channel.name !== 'chatninja') {
@@ -72,13 +70,20 @@ bot.on('interactionCreate', async (interaction) => {
     const username = interaction.user.username;
     const command = interaction.commandName;
 
+    if (process.env.DISABLE_API_KEY === "true") {
+        if (interaction.user.id !== bot.user.id) {
+            await interaction.reply({ content: 'API Key has been disabled. Please try again later.', ephemeral: true });
+        }
+        return;
+    }
+
     const userRef = db.ref(`guilds/${guildId}/users/${userId}`);
 
     const userSnapshot = await userRef.once('value');
     if (!userSnapshot.exists()) {
         await userRef.set({
             username,
-            session: { isActive: false, isProcessing: false, conversationHistory: [] }
+            session: { isActive: false, isProcessing: false, mode: "unactivated", configuring: false, apiKey: "default", trialPrompts: 5, conversationHistory: [] }
         });
     }
 
@@ -95,18 +100,100 @@ bot.on('interactionCreate', async (interaction) => {
     }
 
     if (command === 'ninja') {
+        if (session.mode === "unactivated") {
+            await interaction.reply({ content: 'Please use the /config command to activate ChatNinja first.', ephemeral: true });
+            return;
+        }
         if (session.isActive) {
             await interaction.reply({ content: 'You already have an active session! Start chatting', ephemeral: true });
             return;
         }
 
         await userRef.child('session').update({ isActive: true });
-        interaction.reply({ content: `Session started for ${username}! Type your message to chat.`, ephemeral: true });
+        interaction.reply({ content: `Session active for ${username}! Type your message to chat.`, ephemeral: true });
     } else if (command === 'end') {
+        if (session.mode === "unactivated") {
+            await interaction.reply({ content: 'Please use the /config command to activate ChatNinja first.', ephemeral: true });
+            return;
+        }
         await userRef.child('session').update({ isActive: false, isProcessing: false });
         interaction.reply({ content: 'Your session has ended!', ephemeral: true });
+    } else if (command === 'config') {
+        await userRef.child('session').update({ configuring: true });
+        await interaction.reply({ content: 'Please select a mode to activate ChatNinja:\n\n1.) Trial mode (5 trial prompts)\n\n2.) Provide your own OpenAI API Key\n\n3.) Override access with Access Key\n\nPlease choose 1, 2 or 3', ephemeral: true });
+        const filter = m => m.author.id === interaction.user.id;
+        const collected = await interaction.channel.awaitMessages({ filter, max: 1, time: 60000, errors: ['time'] });
+        const response = collected.first().content;
+
+        if (response === "1") {
+            if (session.trialPrompts === 0) {
+                await interaction.editReply({ content: 'You have used up your free trial. Please provide your own OpenAI API Key or use an Access Key to override access.', ephemeral: true });
+                await userRef.child('session').update({ configuring: false });
+                return;
+            }
+            await interaction.editReply({ content: 'Trial mode activated! You have 5 trial prompts.', ephemeral: true });
+            await userRef.child('session').update({ mode: "trialMode", configuring: false });
+        } else if (response === "2") {
+            await interaction.editReply({ content: 'Please enter your OpenAI API Key:', ephemeral: true });
+            const apiKeyFilter = m => m.author.id === interaction.user.id;
+            const apiKeyCollected = await interaction.channel.awaitMessages({ apiKeyFilter, max: 1, time: 60000, errors: ['time'] });
+            const apiKeyResponse = apiKeyCollected.first().content;
+
+            const test_openai = new OpenAI({ apiKey: apiKeyResponse });
+
+            try {
+                const result = await test_openai.chat.completions.create({
+                    model: 'gpt-4o',
+                    messages: [
+                        {
+                            role: "user",
+                            content: "How many days are there in a week?"
+                        }
+                    ],
+                    max_tokens: 50
+                });
+
+                const assistantReply = result.choices[0].message.content;
+                if (assistantReply) {
+                    await interaction.editReply({ content: 'Your API Key has been verified and activated!', ephemeral: true });
+                    await userRef.child('session').update({ apiKey: apiKeyResponse, mode: "apiKeyMode", configuring: false });
+                }
+            }
+            catch (error) {
+                console.error("Error from OpenAI API's Servers:", error);
+                await interaction.editReply({ content: 'Invalid API Key. Please try again.', ephemeral: true });
+                await userRef.child('session').update({ configuring: false });
+            }
+        } else if (response === "3") {
+            await interaction.editReply({ content: 'Please enter your Access Key:', ephemeral: true });
+            const accessKeyFilter = m => m.author.id === interaction.user.id;
+            const accessKeyCollected = await interaction.channel.awaitMessages({ accessKeyFilter, max: 1, time: 60000, errors: ['time'] });
+            const accessKeyResponse = accessKeyCollected.first().content;
+
+            // check if the access key matches any records under accessKeys
+            const accessKeysRef = db.ref('accessKeys');
+            const accessKeysSnapshot = await accessKeysRef.once('value');
+            const accessKeys = accessKeysSnapshot.val();
+
+            if (accessKeys === null || accessKeys[String(accessKeyResponse)] !== "key") {
+                await interaction.editReply({ content: 'Invalid Access Key. Please try again.', ephemeral: true });
+                await userRef.child('session').update({ configuring: false });
+                return;
+            }
+
+            await accessKeysRef.child(accessKeyResponse).remove();
+            const accessKey = require('uuid').v4();
+            await accessKeysRef.child(accessKey).set("key");
+            await interaction.editReply({ content: 'Access Key verified. Access overriden.', ephemeral: true });
+            await userRef.child('session').update({ mode: "overrideMode", configuring: false });
+
+        } else {
+            await interaction.editReply({ content: 'Please choose an option from 1 to 3', ephemeral: true });
+            await userRef.child('session').update({ configuring: false });
+        }
     } else {
-        await interaction.reply({ content: 'Invalid command. Please use the /ninja command to start a session.', ephemeral: true });
+        await interaction.editReply({ content: 'Invalid command. Please use the /ninja command to start a session.', ephemeral: true });
+        await userRef.child('session').update({ configuring: false });
     }
 });
 
@@ -160,10 +247,26 @@ bot.on('messageCreate', async (message) => {
             await userRef.child('session').set(session);
         }
 
+        if (session.configuring === true) return;
+
+        if (session.mode === "unactivated") return;
+
         if (!session.isActive) {
-            message.reply({ content: 'Please use the /ninja command to start a session.', ephemeral: true });
+            message.reply({ content: "Please use the /ninja command to start a session, or the /config command to activate ChatNinja first if you haven't done so yet.", ephemeral: true });
             return;
         }
+
+        if (session.mode === "trialMode" && session.trialPrompts === 0) {
+            message.reply({ content: "You have exhausted your trial prompts. Please use the /config command to activate ChatNinja.", ephemeral: true });
+            return;
+        }
+
+        let user_openai = null
+        if (session.mode === "apiKeyMode" && session.apiKey !== "default") {
+            user_openai = new OpenAI({ apiKey: session.apiKey });
+        }
+
+        const model = ((user_openai !== null) && (session.mode !== "overrideMode")) ? user_openai : openai;
 
         if (session.isProcessing) {
             message.reply({ content: 'Please wait for ChatNinja to respond to previous messages.', ephemeral: true });
@@ -202,7 +305,7 @@ bot.on('messageCreate', async (message) => {
                 let result;
                 if (imageUrls.length > 0) {
                     // If there are images, send them to the API
-                    result = await openai.chat.completions.create({
+                    result = await model.chat.completions.create({
                         model: 'gpt-4o',
                         messages: [
                             ...conversationLog,
@@ -215,7 +318,7 @@ bot.on('messageCreate', async (message) => {
                     });
                 } else {
                     // If there's no image, just send the text message
-                    result = await openai.chat.completions.create({
+                    result = await model.chat.completions.create({
                         model: 'gpt-4o-mini',
                         messages: conversationLog,
                         max_tokens: 600
@@ -224,6 +327,9 @@ bot.on('messageCreate', async (message) => {
 
                 const assistantReply = result.choices[0].message.content;
                 message.reply(assistantReply);
+                if (session.mode === "trialMode") {
+                    await userRef.child('session').update({ trialPrompts: session.trialPrompts - 1 });
+                }
                 conversationLog.push({ role: 'assistant', content: assistantReply });
                 await userRef.child('session/conversationHistory').set(conversationLog);
             } catch (error) {
